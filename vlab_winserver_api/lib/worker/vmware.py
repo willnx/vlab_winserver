3266b57b09e51e45eda6b211b83fceb50b615856
willnx <willnx84@gmail.com>
2018-12-07 11:17:14 -0800

    better error message when given invalid image/version to deploy

--- vlab_winserver_api/lib/worker/vmware.py
+++ vlab_winserver_api/lib/worker/vmware.py
@@ -90,7 +90,11 @@ def create_winserver(username, machine_name, image, network, logger):
                  password=const.INF_VCENTER_PASSWORD) as vcenter:
         image_name = convert_name(image)
         logger.info(image_name)
-        ova = Ova(os.path.join(const.VLAB_WINSERVER_IMAGES_DIR, image_name))
+        try:
+            ova = Ova(os.path.join(const.VLAB_WINSERVER_IMAGES_DIR, image_name))
+        except FileNotFoundError:
+            error = 'Invalid version of Windows Server supplied: {}'.format(image)
+            raise ValueError(error)
         try:
             network_map = vim.OvfManager.NetworkMapping()
             network_map.name = ova.networks[0]
